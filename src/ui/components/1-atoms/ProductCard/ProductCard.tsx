1679db1d2a29099e300ba8fb46a1c5b9d31fd462
asef0024 <asef0024@stud.kea.dk>
2022-11-23 10:52:36 +0100

    card

---
+++ src/ui/components/1-atoms/ProductCard/ProductCard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "../Button";
+import styles from "./ProductCard.module.scss";
+// import { Icon } from "ui/components"
+
+export interface ButtonProps {
+  title?: string;
+  text?: React.ReactNode;
+  img: string;
+}
+
+export const ProductCard: React.FC<ButtonProps> = (props) => {
+  return (
+    <div className="card">
+      <div className="card_body">
+        <img src={props.img} alt="" />
+        <h2 className="card_title">{props.title}</h2>
+        <p className="card_text">{props.text}</p>
+      </div>
+      <Button></Button>
+    </div>
+  );
+};
