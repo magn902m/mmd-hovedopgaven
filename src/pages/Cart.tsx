37d2f8e33ad6ac1de3b2e79549ba3c906fb0afa6
asef0024 <asef0024@stud.kea.dk>
2022-12-07 17:41:12 +0100

    cart css done

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -11,32 +11,36 @@ export const Cart = () => {
 
   return (
     <main className="nets_cart">
-      <header className="nets_cart_header">
-        <h1>Din indkøbskurv</h1>
+      <section className="nets_cart_section">
+        <h3>Din indkøbskurv</h3>
         <Link to={`/products`}>Tilbage til produkter</Link>
-      </header>
+      </section>
 
       <section className="nets_cart_grid">
         <div className="nets_cart_grid_header">
           <h4 className="grid_header_product">Produkt</h4>
-          <h4 className="grid_header_price">Pris</h4>
-          <h4 className="grid_header_amount">Antal</h4>
-          <h4 className="grid_header_total">I alt</h4>
+          {/* <div className="grid_header_mobile_none">
+            <h4 className="grid_header_price">Pris</h4>
+            <h4 className="grid_header_amount">Antal</h4>
+          </div> */}
+          <h4 className="grid_header_subtotal">Total pris</h4>
         </div>
-        <div>
+
+        <div className="nets_cart_grid_body">
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
         </div>
+
         <div className="nets_cart_grid_footer">
-          <p>I alt</p>
-          <p>
+          <h4>I alt</h4>
+          <h4 className="grid_footer_total">
             {cartItems.reduce((total, cartItem) => {
               const item = WebshopItems.find((i) => i.id === cartItem.id);
               return total + (item?.price || 0) * cartItem.quantity;
             }, 0)}
             kr. pr. måned
-          </p>
+          </h4>
         </div>
       </section>
 
