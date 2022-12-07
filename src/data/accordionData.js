b8592d4e53b78b730e1074a5a7a131996d00b848
Magnus <magn902m@stud.kea.dk>
2022-12-07 15:29:42 +0100

    improved the accordion

---
+++ src/data/accordionData.js
@@ -0,0 +1,58 @@
+export const accordionData = [
+  {
+    title: "Information om pakken",
+    content: `<p>Information omkring pakke og alle fordelene:</p>
+      <br />
+      <ul>
+        <li>Transaktionsgebyr: Fra 0,69 % pr. transaktion.</li>
+        <li>Analytics PRO: Sammenlign din forretning med andre i din branche.</li>
+        <li>Abonnement på bonruller: Modtag 20 bonruller i kvartalet</li>
+        <li>
+          Udbetaling: Hver anden dag på alle de internationale kortbrands – og med Dankort er
+          der daglig udbetaling
+        </li>
+        <li>Kundeservice: 24/7</li>
+        <li>Ombytningsservice på din terminal: På din adresse inden for 4 timer.</li>
+        <li>
+          Gratis kundeindsigt: App og portal med adgang til dine salgs- og transaktionsdata
+        </li>
+        <li>
+          Indløsningsaftale der giver dig mulighed for at modtage betalinger med de mest
+          populære kort
+        </li>
+        <li>Bindingsperiode: 36 måneder</li>
+      </ul>
+      <p>
+        *Intervaller er vejledende og afhænger af dit specifikke kortmix – og Dankort er
+        særskilt
+      </p>`,
+  },
+  {
+    title: "Hvad vil du gøre for klimaet?",
+    content: `<p>Gør noget godt for os alle:</p>
+      <p>
+        Som en del af de produkter du køber, går 1% af dit køb til at kompensere for det
+        klimaaftryk produkter har og vil lave i fremtiden. Se mere omkring
+        <a href="/green-goal">vores grønne mål</a>.
+      </p>
+
+      <ul>
+        <li>Mulighed for at kompensere mere end 1% for at nutralisere mere CO2</li>
+        <li>Juster nemt og hurtigt</li>
+        <li>Få gratis tilpasning af dit produkt med i købet, hvis du kompensere mere end 5%</li>
+      </ul>`,
+  },
+  {
+    title: "Tilpas efter din visuelle identitet",
+    content: `<p>
+        Det er vigtig for os, at de produkter de køber passer ind i din visuelle identitet, så
+        det passer med resten af din virksomhed. Derfor har du nu mulighed for at lave en række
+        ændringer til dine produkter.
+      </p>
+      <br />
+      <p>
+        Hvis du kompensere for 5% eller mere for at kompensere for dit CO2 aftryk, får man
+        gratis tilpasning af dit produkt med i købtet
+      </p>`,
+  },
+];
--- src/ui/components/1-atoms/Accordion/AccordionItem/AccordionItem.tsx
+++ src/ui/components/1-atoms/Accordion/AccordionItem/AccordionItem.tsx
@@ -2,24 +2,43 @@ import React, { useState } from "react";
 import styles from "./AccordionItem.module.scss";
 import CloseIcon from "../../../../../assets/icons/close.svg";
 import PlusIcon from "../../../../../assets/icons/plus.svg";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export interface AccordionItemProps {
   title: string;
-  content: React.ReactNode;
+  content: string;
 }
 
 export const AccordionItem: React.FC<AccordionItemProps> = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
+
+  console.log(content);
 
   return (
     <article className={styles.AccordionItem}>
       <div className={styles.AccordionTitle} onClick={() => setIsActive(!isActive)}>
         <h5>{title}</h5>
         <div className={styles.AccordionIcons}>
-          {isActive ? <img src={CloseIcon} alt="" /> : <img src={PlusIcon} alt="" />}
+          {/* {isActive ? <img src={CloseIcon} alt="" /> : <img src={PlusIcon} alt="" />} */}
+          {
+            <div className={isActive ? styles.AccordionItem_open : styles.AccordionItem_close}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-down"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
+              </svg>
+            </div>
+          }
         </div>
       </div>
-      {isActive && <div className={styles.AccordionContent}>{content}</div>}
+      {isActive && (
+        <div className={styles.AccordionContent} dangerouslySetInnerHTML={{ __html: content }} />
+      )}
     </article>
   );
 };
