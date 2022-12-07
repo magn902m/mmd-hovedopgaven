7d0d40b1a25db5da09a785c50d9af4fd43bed86c
asef0024 <asef0024@stud.kea.dk>
2022-12-07 18:37:32 +0100

    scroll to top done

---
+++ src/hooks/scrollToTop.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+export const ScrollToTop = (props: any) => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{props.children}</>;
+};
