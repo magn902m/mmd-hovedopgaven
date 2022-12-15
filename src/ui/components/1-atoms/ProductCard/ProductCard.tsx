06cb1c961d20076ee0bcc88fe4c35f10e3fbe96d
Magnus <magn902m@stud.kea.dk>
2022-12-15 17:12:37 +0100

    text center products page, images can be clicked, to go til product page

--- src/ui/components/1-atoms/ProductCard/ProductCard.tsx
+++ src/ui/components/1-atoms/ProductCard/ProductCard.tsx
@@ -21,7 +21,7 @@ export const ProductCard: React.FC<CardProps> = (props) => {
     //   <Button className="nets_card_button" label={props.btnLabel} onClick={props.onClick}></Button>
     // </article>
     <article className="nets_card">
-      <img className="nets_card_image" src={props.productInfo.img} />
+      <img className="nets_card_image" src={props.productInfo.img} onClick={props.onClick} />
       <div className="text_content">
         <h3 className="nets_card_title">{`${props.productInfo.name}: Fra ${props.productInfo.transactionInfo}% pr. transaktion`}</h3>
         <p className="nets_card_text">{props.productInfo.desc2}</p>
