b21b4a71d69afc898af2276a1312afd69042f60d
Magnus <magn902m@stud.kea.dk>
2022-12-15 14:14:00 +0100

    styling progress bar

--- src/ui/components/3-organisms/Checkout/ProgressBar/ProgressBar.tsx
+++ src/ui/components/3-organisms/Checkout/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./ProgressBar.module.scss";
 
 export const ProgressBar = ({ toggleCheckoutArr }: any) => {
@@ -6,18 +7,26 @@ export const ProgressBar = ({ toggleCheckoutArr }: any) => {
     <section className={styles.ProgressBar_container}>
       {/* Here are we checking if the current number is equal to isCurrent and changing a class that way */}
       <ul className={styles.ProgressBar_steps_container}>
+        <li>
+          <Link to="/cart" className={styles.ProgressBar_to_cart}>
+            Indkøbskurv
+          </Link>
+        </li>
+        <li>{">"}</li>
         <li
           className={styles.ProgressBar_step_item}
           onClick={() => {
-            toggleCheckoutArr.setToggleCheckoutInformation(true);
-            toggleCheckoutArr.setToggleCheckoutDelivery(false);
-            toggleCheckoutArr.setToggleCheckoutOverview(false);
-            toggleCheckoutArr.setIsCurrent(1);
+            if (toggleCheckoutArr.isCurrent === 2 || toggleCheckoutArr.isCurrent === 3) {
+              toggleCheckoutArr.setToggleCheckoutInformation(true);
+              toggleCheckoutArr.setToggleCheckoutDelivery(false);
+              toggleCheckoutArr.setToggleCheckoutOverview(false);
+              toggleCheckoutArr.setIsCurrent(1);
+            }
           }}
         >
           <p
             className={`${styles.ProgressBar_progress_count} ${
-              toggleCheckoutArr.isCurrent === 1 ? styles.ProgressBar_current_item : ""
+              toggleCheckoutArr.isCurrent === 1 ? styles.ProgressBar_current_item : null
             }`}
           >
             Oplysninger
@@ -27,10 +36,12 @@ export const ProgressBar = ({ toggleCheckoutArr }: any) => {
         <li
           className={styles.ProgressBar_step_item}
           onClick={() => {
-            toggleCheckoutArr.setToggleCheckoutInformation(false);
-            toggleCheckoutArr.setToggleCheckoutDelivery(true);
-            toggleCheckoutArr.setToggleCheckoutOverview(false);
-            toggleCheckoutArr.setIsCurrent(2);
+            if (toggleCheckoutArr.isCurrent === 3) {
+              toggleCheckoutArr.setToggleCheckoutInformation(false);
+              toggleCheckoutArr.setToggleCheckoutDelivery(true);
+              toggleCheckoutArr.setToggleCheckoutOverview(false);
+              toggleCheckoutArr.setIsCurrent(2);
+            }
           }}
         >
           <p
@@ -42,15 +53,7 @@ export const ProgressBar = ({ toggleCheckoutArr }: any) => {
           </p>
         </li>
         <li>{">"}</li>
-        <li
-          className={styles.ProgressBar_step_item}
-          onClick={() => {
-            toggleCheckoutArr.setToggleCheckoutInformation(false);
-            toggleCheckoutArr.setToggleCheckoutDelivery(false);
-            toggleCheckoutArr.setToggleCheckoutOverview(true);
-            toggleCheckoutArr.setIsCurrent(3);
-          }}
-        >
+        <li className={styles.ProgressBar_step_item}>
           <p
             className={`${styles.ProgressBar_progress_count} ${
               toggleCheckoutArr.isCurrent === 3 ? styles.ProgressBar_current_item : ""
