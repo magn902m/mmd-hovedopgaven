ef4f519588710ea0b7199f321f4b3862af682ee5
Magnus <magn902m@stud.kea.dk>
2022-12-28 23:21:35 +0100

    made, so the user can see the image or not and trying to implement a image

--- src/ui/components/3-organisms/PaymentTerminalCanvas/Environment.jsx
+++ src/ui/components/3-organisms/PaymentTerminalCanvas/Environment.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/no-unknown-property */
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useRef, useContext } from "react";
 import { extend, useThree, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Stage } from "@react-three/drei";
 import * as THREE from "three";
 import { PaymentTerminalModel } from "./PaymentTerminalModel";
+import { ThreeJSContext } from "../../../../contexts/ThreeJSContext";
+
 // import { Cube } from "./Cube";
 
 extend({ OrbitControls });
@@ -13,9 +15,13 @@ export const Environment = () => {
   const { camera, gl } = useThree();
   const ref = useRef();
 
+  const { updateModel } = useContext(ThreeJSContext);
+  console.log(updateModel.imageUrl);
+
   const texture = useLoader(
     THREE.TextureLoader,
     process.env.PUBLIC_URL + "/images/nets_logo_white.svg"
+    // updateModel.imageUrl
   );
 
   return (
@@ -26,11 +32,12 @@ export const Environment = () => {
       <ambientLight intensity={0.5} />
 
       {/* <Cube /> */}
-      <mesh position={[0, 0.3, 0.33]} scale={[0.3, 0.3, 0.3]}>
-        <planeBufferGeometry attach="geometry" />
-        <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
-      </mesh>
-
+      {updateModel.showImage ? (
+        <mesh position={[0, 0.3, 0.33]} scale={[0.3, 0.3, 0.3]}>
+          <planeBufferGeometry attach="geometry" />
+          <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
+        </mesh>
+      ) : null}
       <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
         <Suspense fallback={null} r3f>
           <PaymentTerminalModel scale={0.35} />
