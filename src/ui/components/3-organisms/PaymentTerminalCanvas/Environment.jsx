19702b426b9a5384d580885af60b80ddd161c551
Magnus <magn902m@stud.kea.dk>
2022-12-28 22:17:45 +0100

    setup for the image on the model

--- src/ui/components/3-organisms/PaymentTerminalCanvas/Environment.jsx
+++ src/ui/components/3-organisms/PaymentTerminalCanvas/Environment.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/no-unknown-property */
 import React, { Suspense, useRef } from "react";
-import { extend, useThree } from "@react-three/fiber";
+import { extend, useThree, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Stage } from "@react-three/drei";
+import * as THREE from "three";
 import { PaymentTerminalModel } from "./PaymentTerminalModel";
 // import { Cube } from "./Cube";
 
@@ -12,6 +13,11 @@ export const Environment = () => {
   const { camera, gl } = useThree();
   const ref = useRef();
 
+  const texture = useLoader(
+    THREE.TextureLoader,
+    process.env.PUBLIC_URL + "/images/nets_logo_white.svg"
+  );
+
   return (
     <>
       <orbitControls args={[camera, gl.domElement]} />
@@ -20,6 +26,11 @@ export const Environment = () => {
       <ambientLight intensity={0.5} />
 
       {/* <Cube /> */}
+      <mesh position={[0, 0.3, 0.33]} scale={[0.3, 0.3, 0.3]}>
+        <planeBufferGeometry attach="geometry" />
+        <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
+      </mesh>
+
       <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
         <Suspense fallback={null} r3f>
           <PaymentTerminalModel scale={0.35} />
