fe5982785994cc66b19b2e03550c969d7ea3debe
Magnus <magn902m@stud.kea.dk>
2023-01-17 14:11:54 +0100

    fixed footer links

--- src/ui/components/4-habitats/Footer/Footer.tsx
+++ src/ui/components/4-habitats/Footer/Footer.tsx
@@ -7,49 +7,37 @@ export const Footer = () => {
       <h3>Genveje links</h3>
       <ul className="sts_footer_menu">
         <li className="sts_footer_item">
-          <a href="https://www.sts.eu/da-DK/payments/acquiring" className="sts_footer_links active">
+          <a href="/" className="sts_footer_links active">
             Indløsningsaftale
           </a>
         </li>
         <li className="sts_footer_item">
-          <a href="https://www.sts.eu/da-DK/payments/online" className="sts_footer_links active">
+          <a href="/" className="sts_footer_links active">
             Betaling online
           </a>
         </li>
         <li className="sts_footer_item">
-          <a
-            href="https://www.sts.eu/da-DK/payments/value-added-services"
-            className="sts_footer_links active"
-          >
+          <a href="/" className="sts_footer_links active">
             Tillægsydelser
           </a>
         </li>
         <li className="sts_footer_item">
-          <a
-            href="https://www.sts.eu/da-DK/payments/value-added-services/financing"
-            className="sts_footer_links active"
-          >
+          <a href="/" className="sts_footer_links active">
             Sts Financing
           </a>
         </li>
         <li className="sts_footer_item">
-          <a
-            href="https://www.sts.eu/da-DK/payments/value-added-services/insights-and-reporting"
-            className="sts_footer_links active"
-          >
+          <a href="/" className="sts_footer_links active">
             Indsigt & rapportering
           </a>
         </li>
         <li className="sts_footer_item">
-          <a
-            href="https://www.sts.eu/Pages/Terms-and-conditions---Cookie-and-Privacy-Policy-DK.aspx"
-            className="sts_footer_links active"
-          >
+          <a href="/" className="sts_footer_links active">
             Cookiepolitik
           </a>
         </li>
         <li className="sts_footer_item">
-          <a href="https://www.sts.eu/GDPR" className="sts_footer_links active">
+          <a href="/" className="sts_footer_links active">
             GDPR regulation
           </a>
         </li>
